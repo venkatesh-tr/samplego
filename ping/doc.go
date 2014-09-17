2d507b056dac426b331b3432b0e84a0846829c79
Venkatesh T R <venkatesha.ramaiah@hirepro.in>
2014-09-17 16:58:57 +0530

    added documentation comments

---
+++ ping/doc.go
@@ -0,0 +1,7 @@
+/* ping - ping package is a sample package
+
+depends on https://wiki.ubuntu.com/goamz
+
+Written by Venkatesha T R <venkatesha.ramaiah@hirepro.in>
+*/
+package ping
--- ping/ping.go
+++ ping/ping.go
@@ -1,3 +1,12 @@
+//
+// ping - ping package is a sample package
+//
+// depends on https://wiki.ubuntu.com/goamz
+//
+//
+// Written by Venkatesha T R <venkatesha.ramaiah@hirepro.in>
+//
+
 package ping
 
 import (
